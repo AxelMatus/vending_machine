8d90d10c5aedb9cba58e24a8541a3c49033e9a0f
AxelMatus <axelantonio@gmail.com>
2022-07-26 03:20:27 -0600

    Added Machine Screen

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,15 @@ import styles from '../styles/Home.module.css'
 import NumericKeyBoard from '../components/Numeric_keyboard'
 import AmountKeyBoard from '../components/Money_amount_keyboard'
 
-export default function Home() {
+export default function Controller() {
   return (
     <div  className="flex">
       <div className="bg-main-gray h-screen w-2/3">
-
+        <div className="bg-secundary-gray border-black  border-[24px] rounded-md m-10 h-5/6">
+          <div className="bg-main-green rounded-md h-12 text-2xl font-bebasNeue text-white p-2" >
+            MATUS VENDING MACHINE
+          </div>
+        </div>
       </div>
       <div className="bg-main-gray h-screen w-1/3">
         <NumericKeyBoard width={"w-40"} height={"h-40"} padding={"pt-20"}/>
