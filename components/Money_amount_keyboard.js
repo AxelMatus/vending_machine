570046c11363abe17ac262b7d33ba7c051924a4b
AxelMatus <axelantonio@gmail.com>
2022-07-26 02:51:29 -0600

    added new component money keyboard

---
+++ components/Money_amount_keyboard.js
@@ -0,0 +1,51 @@
+import Button from './Button'
+import { useState } from 'react';
+
+const create_buttons = (value, handleClick) => {
+    const handleButtonClick = (event) => handleClick(event);
+
+    return(
+        <Button key={value.id} width={value.width} height={value.height} bg_color={"bg-white"} text_color={"text-black"} text={value.tag} value={value.value} text_size={"text-2xl font-bold"} handleClick={handleButtonClick}/>
+    )
+}
+
+const create_rows = (values) => {
+    return (
+        <div className='flex justify-center'>
+            { values.map((value) => {
+                return create_buttons(value, value.handleClick);
+            })}
+        </div>
+);}
+
+export default function Money_amount_keyboard (props) {
+    const [userCash, setUserCash] = useState(0);
+
+    const handleUserInput = (event) => {
+        event.preventDefault();
+        setUserCash(userCash + parseInt(event.target.value));
+    }
+
+    const firstRow_buttons = [
+        { id: 1, value: "1000", tag: "₡1.000", width: "w-40", height:"h-12", handleClick: handleUserInput},
+    ]
+
+    const secondRow_buttons = [
+        { id: 2, value: "500", tag: "₡500", width: "w-20", height:"h-12", handleClick: handleUserInput},
+        { id: 3, value: "100", tag: "₡100", width: "w-20", height:"h-12", handleClick: handleUserInput},
+    ]
+
+    const thirdRow_buttons = [
+        { id: 4, value: "50", tag: "₡50", width: "w-20", height:"h-12", handleClick: handleUserInput},
+        { id: 5, value: "25", tag: "₡25", width: "w-20", height:"h-12", handleClick: handleUserInput},
+    ]
+    
+    console.log(userCash);
+    return(
+        <div className={`w-${props.width} h-${props.height} ${props.padding}`}>
+                {create_rows(firstRow_buttons)}
+                {create_rows(secondRow_buttons)}
+                {create_rows(thirdRow_buttons)}
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import NumericKeyBoard from '../components/Numeric_keyboard'
+import AmountKeyBoard from '../components/Money_amount_keyboard'
 
 export default function Home() {
   return (
     <div  className="flex">
-    <div className="bg-main-gray h-screen w-2/3">
+      <div className="bg-main-gray h-screen w-2/3">
 
-    </div>
-    <div className="bg-main-gray h-screen w-1/3">
-      <NumericKeyBoard width={"40"} height={"40"} padding={"p-20"}/>
-    </div>
+      </div>
+      <div className="bg-main-gray h-screen w-1/3">
+        <NumericKeyBoard width={"w-40"} height={"h-40"} padding={"pt-20"}/>
+        <AmountKeyBoard width={"w-40"} height={"h-40"} padding={"p-8"}/>
+      </div>
     </div>
   )
 }
