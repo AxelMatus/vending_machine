77a037bd79255b5c6b66b4808f4149644e2d04cc
AxelMatus <axelantonio@gmail.com>
2022-07-26 23:25:58 -0600

    minor changes

--- components/User_bill.js
+++ components/User_bill.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import {get_total_sum_of_products, get_total_amount} from '../logic/machine_calculations'
 
 export default function User_bill (props) {
-    let user_product_list = get_total_sum_of_products(props.user_products);
-    let total_amount = get_total_amount(user_product_list);
+    const [userProductList, setUserProductList] = useState(get_total_sum_of_products(props.user_products));
+    const [totalAmount, setTotalAmount] = useState (get_total_amount(userProductList));
 
     return(
         <div className="mt-16 pl-12 w-1/2 text-white">
-            { user_product_list.map((product) => {
+            { userProductList.map((product) => {
                 return (
                 <div key={product.id} className="flex">
                     <p className='w-1/2'> {`${product.name} x ${product.quantity}`} </p>
@@ -18,7 +18,7 @@ export default function User_bill (props) {
             <hr className='bg-white w-full my-2'/>
             <div className='flex'>
                 <p className='w-1/2'> Total: </p>
-                <p className='w-1/2 text-right'> {total_amount} </p>
+                <p className='w-1/2 text-right'> {totalAmount} </p>
             </div>
         </div>
     );
