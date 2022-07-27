599de751ad842dbabf7456525e450098ecff62d0
AxelMatus <axelantonio@gmail.com>
2022-07-27 17:53:45 -0600

    fixed errors in logic

--- logic/machine_calculations.js
+++ logic/machine_calculations.js
@@ -2,24 +2,17 @@ import { products } from '../data/products';
 import { coins } from '../data/money'
 
 export function get_total_sum_of_products (product_list) {
-    let user_product_list = []
+    let user_product_list = [];
     product_list.map((product) =>{
-        const product_found = user_product_list.find(element => element.name === product.name);
         const product_information = products.find(element => element.name === product.name);
-
-        if(product_found){
-            product_found.quantity += 1;
-            product.price += product_information.price;
-        } else {
             const new_user_product = {
                 id: product.id,
                 name: product.name,
                 quantity: product.quantity,
-                price: product.price,
+                price: product_information.price * product.quantity,
             }
             user_product_list.push(new_user_product);
-        }
-    })
+    });
     return user_product_list;
 }
 
@@ -35,29 +28,31 @@ export function calculate_change_detailed (user_balance, amount_to_pay) {
     let change = [];
     let coins_sum = 0;
     const change_amount = calculate_change(user_balance, amount_to_pay);
-    if(user_balance===amount_to_pay){
-        return change;
-    }
-    coins.map((coin) => {
-
-        let change_coin = {
-            id: coin.id,
-            value: coin.value,
-            quantity: 0
+    if(change_amount >= 0) {
+        if(user_balance===amount_to_pay){
+            return change;
         }
-        
-        let can_add_another_coin = coins_sum + coin.value <= change_amount;
-        while(can_add_another_coin && coins_sum < change_amount && coin.quantity > 0){
+        coins.map((coin) => {
 
-            coins_sum += coin.value;
-            coin.quantity -= 1;
-            change_coin.quantity += 1;
-            if(coins_sum + coin.value > change_amount){
-                can_add_another_coin = false; 
+            let change_coin = {
+                id: coin.id,
+                value: coin.value,
+                quantity: 0
             }
-        }
-        change.push(change_coin);
-    })
+            
+            let can_add_another_coin = coins_sum + coin.value <= change_amount;
+            while(can_add_another_coin && coins_sum < change_amount && coin.quantity > 0){
+
+                coins_sum += coin.value;
+                coin.quantity -= 1;
+                change_coin.quantity += 1;
+                if(coins_sum + coin.value > change_amount){
+                    can_add_another_coin = false; 
+                }
+            }
+            change.push(change_coin);
+        })
+    }
     return change;
 }
 
